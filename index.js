81c3774b08cf5786f4be48b92c5ac3579cb19d8b
QD2022 <104011263+QD2022@users.noreply.github.com>
2022-10-27 14:30:47 +0200

    index added

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+let parentContainer = document.querySelector(".container");
+let circles = document.querySelectorAll(".circle");
+let specCircle = document.querySelector("#special-circle");
 
+// console.log(parentContainer);
+// console.log(circles);
+// console.log(specCircle);
+
+parentContainer.addEventListener("click", (e) => {
+  for (let i = 0; i < circles.length; i++) {
+    if (e.target === circles[i]) {
+      e.target.remove();
+    }
+  }
+  console.log("event1");
+  
+});
+
+// circles.addEventListener("click", (e)=>{
+//     console.log(e.target);
+//     console.log(e.currentTarget);
+// })
+
+specCircle.addEventListener(
+  "click",
+  (e) => {
+    console.log(e.target);
+    specCircle.style.cssText = "background-color: green";
+    e.stopPropagation();
+  },true
+
+);
